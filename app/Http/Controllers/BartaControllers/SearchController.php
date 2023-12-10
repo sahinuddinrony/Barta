218374681c4515f55cd03493ec8f498b90f2f55b
Sahin Uddin Rony <sahincseiu@gmail.com>
2023-12-10 15:52:11 +0600

    Second Update

--- app/Http/Controllers/BartaControllers/SearchController.php
+++ app/Http/Controllers/BartaControllers/SearchController.php
@@ -18,24 +18,25 @@ class SearchController extends Controller
         $query = $request->input('query');
 
         // Search for users based on the query
-        $user = User::where('name', 'like', "%$query%")
+        $userData = User::where('name', 'like', "%$query%")
             ->orWhere('lastname', 'like', "%$query%")
             ->orWhere('username', 'like', "%$query%")
             ->orWhere('email', 'like', "%$query%")
             ->first();
-
-            if($user) {
+$id = $userData->id;
+            if($userData) {
                 // Eager load user's posts with user, comments, and comments.user information
                 $userPosts = Post::with(['user:id,name,username', 'comments', 'comments.user:id,name,username'])
-                    ->where('user_id', $user->id)
+                    ->where('user_id', $userData->id)
                     ->orderByDesc('created_at')
                     ->get();
 
                 // Load user's comments
-                $userComments = Comment::where('user_id', $user->id)->get();
+                $userComments = Comment::where('user_id', $userData->id)->get();
 
                 // Pass the data to the view
-                return view('barta.pages.profile.profile', compact('user', 'userPosts', 'userComments'));
+                return view('barta.pages.profile.view_single_profile', compact('userData', 'userPosts', 'id', 'userComments'));
+                // return view('barta.pages.profile.profile', compact('user', 'userPosts', 'userComments'));
             } else {
                 // Redirect to login if the user is not authenticated
                 return redirect()->route('login')->with('error', 'Please log in to view your profile.');
